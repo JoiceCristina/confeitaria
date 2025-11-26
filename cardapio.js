b759b36b67b4cb6644308dcad6d860f5e5b7f5e6
JoiceCristina <134727130+JoiceCristina@users.noreply.github.com>
2025-11-26 20:14:30 -0300

    criando iten com js

--- cardapio.js
+++ cardapio.js
@@ -1,1 +1,32 @@
-alert('oi fui carregado')
+//Vamos criar os elementos
+//do html om javascript
+//dinamicamente
+
+function criarItemCardapio(titulo, descricao, foto) {
+    const divItemCArdapio = document.createElement('div')
+    divItemCArdapio.className = 'item-carpadio'
+
+    const h3Titulo = document.createElement('h3')
+    h3Titulo.textContent = titulo
+
+    const pDescricao = document.createElement('p')
+    pDescricao.textContent = pDescricao
+    pDescricao.className = 'desricao'
+
+    const img = document.createElement('img')
+    img.src = foto
+    img.className = 'img-item'
+
+    //adicionando os elementos na div mãe
+    const divC = document.getElementById('cardapio')
+
+    divItemCArdapio.appendChild(h3Titulo)
+    divItemCArdapio.appendChild(pDescricao)
+    divItemCArdapio.appendChild(img)
+
+    divC.appendChild(divItemCArdapio)
+}
+//executando a função
+criarItemCardapio(
+    'Bolo de Chocolate', 'Um clássico irresistível com camadas de chocolate', 'https://www.comidaereceitas.com.br/img/sizeswp/1200x675/2020/05/bolo_chocolate_leite.jpg'
+)
