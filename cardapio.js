c6911d07150f2b30a148d043dead31d4a5412d1c
JoiceCristina <134727130+JoiceCristina@users.noreply.github.com>
2025-12-03 19:58:40 -0300

    api render agora

--- cardapio.js
+++ cardapio.js
@@ -32,3 +32,24 @@ criarItemCardapio(
     'Um clássico irresistível com camadas de chocolate', 
     'https://www.comidaereceitas.com.br/img/sizeswp/1200x675/2020/05/bolo_chocolate_leite.jpg'
 )
+
+function buscarBolos() {
+    const url = 'https://dashboard.render.com/static/srv-d45t3oc9c44c73cb03og'; // Substitua com a URL real
+
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erro na requisição: ' + response.statusText);
+            }
+            return response.json(); // Converte a resposta para JSON
+        })
+        .then(dados => {
+            console.log('Lista de bolos:', dados); // Exibe os dados no console
+        })
+        .catch(error => {
+            console.error('Erro ao fazer a requisição:', error); // Trata erros
+        });
+}
+
+// Chamada da função para buscar os bolos
+buscarBolos();
